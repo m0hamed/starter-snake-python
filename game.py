97d7b1bc1a1d8069fa352208a8fc057ad13e4669
Mohamed Heikal <mheikal@google.com>
2020-09-02 16:38:58 -0400

    Better ranking

--- game.py
+++ game.py
@@ -1,26 +1,57 @@
 #!/usr/bin/env python3
 
+from collections import namedtuple
+
 DEFAULT_BOARD_SIZE = 11
-FOOD = 1 << 0
+# FOOD has to be lowest for easy flood_fill
 SNAKE = 1 << 1
 HEAD = 1 << 2
 MY_SNAKE = 1 << 3
 
 POSSIBLE_MOVES = {
-    'up': (0,1),
-    'down': (0,-1),
-    'left': (-1,0),
-    'right': (1,0),
+    'up': (0, 1),
+    'down': (0, -1),
+    'left': (-1, 0),
+    'right': (1, 0),
 }
 
 STAY_ALIVE = 0
-GET_FOOD = 1
+STARVING = 1
 
-def get_empty_board(board_size):
+
+def get_empty_board(board_size, fill=0):
     board = []
     for _ in range(board_size):
-        board.append([0]*board_size)
+        board.append([fill] * board_size)
     return board
+
+
+def clone_board(old_board):
+    new_board = get_empty_board(len(old_board))
+    for i in range(len(old_board)):
+        for j in range(len(old_board[i])):
+            new_board[i][j] = old_board[i][j]
+    return new_board
+
+
+def flood_count(real_board, x, y):
+    R = C = len(real_board)
+    fake_board = clone_board(real_board)
+    if fake_board[x][y] > 0: return 0
+
+    def dfs(r, c):
+        count = 0
+        if fake_board[r][c] == 0:
+            fake_board[r][c] = 1
+            count += 1
+            if r >= 1: count += dfs(r - 1, c)
+            if r + 1 < R: count += dfs(r + 1, c)
+            if c >= 1: count += dfs(r, c - 1)
+            if c + 1 < C: count += dfs(r, c + 1)
+        return count
+
+    return dfs(x, y)
+
 
 class Game:
     def __init__(self, game_state):
@@ -28,6 +59,8 @@ class Game:
         self.my_head_y = None
         self.my_snake = None
         self.my_health = None
+        self.food_board = None
+
         self.board_size = game_state['board']['height']
         self.board = get_empty_board(self.board_size)
         self._add_food(game_state['board']['food'])
@@ -35,9 +68,31 @@ class Game:
         self._set_my_snake(game_state['you'])
 
     def _add_food(self, food_pos):
+        self.food_board = get_empty_board(self.board_size)
+        self.flood_fill(self.food_board, food_pos)
+
+    def flood_fill(self, board, food_pos):
+        queue = []
+        visited = get_empty_board(len(board))
         for pos in food_pos:
-            self.set_at(pos['x'], pos['y'], FOOD)
-    
+            x, y = (pos['x'], pos['y'])
+            queue.append((x, y))
+            self.set_at(x, y, 0, board=board)
+            self.set_at(x, y, 1, board=visited)
+
+        def bfs(q, distance):
+            x, y = q.pop(0)
+            for move, delta in POSSIBLE_MOVES.items():
+                new_x = x + delta[0]
+                new_y = y + delta[1]
+                if self.is_out_of_bounds(new_x, new_y):
+                    continue
+                if not self.get_at(new_x, new_y, board=visited):
+                    self.set_at(x, y, distance, board=board)
+                    self.set_at(x, y, 1, board=visited)
+                    q.append((new_x, new_y))
+        bfs(queue, 1)
+
     def _add_snakes(self, snakes):
         for snake in snakes:
             for pos in snake['body']:
@@ -52,23 +107,27 @@ class Game:
         self.my_head_x = snake['head']['x']
         self.my_head_y = snake['head']['y']
         self.my_snake = snake
-    
+
     def to_board_idx(self, x, y):
-        board_y = self.board_size-y-1
+        board_y = self.board_size - y - 1
         board_x = x - 1
         return board_y, board_x
-    
-    def set_at(self, x, y, value, append=False):
+
+    def set_at(self, x, y, value, append=False, board=None):
+        if board is None:
+            board = self.board
         i, j = self.to_board_idx(x, y)
         if append:
-            self.board[i][j] |= value
+            board[i][j] |= value
         else:
-            self.board[i][j] = value
+            board[i][j] = value
 
-    def get_at(self, x, y):
+    def get_at(self, x, y, board=None):
+        if board is None:
+            board = self.board
         i, j = self.to_board_idx(x, y)
-        return self.board[i][j]
-    
+        return board[i][j]
+
     def get_safe_move(self):
         for move, delta in POSSIBLE_MOVES.items():
             new_x = self.my_head_x + delta[0]
@@ -77,21 +136,22 @@ class Game:
                 continue
             if self.is_empty(new_x, new_y):
                 return move
-    
+
     def is_out_of_bounds(self, x, y):
         if x < 0 or x >= self.board_size:
             return True
         if y < 0 or y >= self.board_size:
             return True
         return False
-    
+
     def is_empty(self, x, y):
         return self.get_at(x, y) == 0
 
     def has_snake(self, x, y):
         return bool(self.get_at(x, y) & SNAKE)
-    
+
     def is_close_to_other_head(self, x, y):
+        count = 0
         for move, delta in POSSIBLE_MOVES.items():
             new_x = x + delta[0]
             new_y = y + delta[1]
@@ -99,33 +159,45 @@ class Game:
                 continue
             value = self.get_at(new_x, new_y)
             if value & HEAD and not (value & MY_SNAKE):
-                return True
+                count += 1
+        return count
 
-    def rank_moves(self, goal=STAY_ALIVE):
-        move_rank = {}
+    def get_possible_moves(self):
+        possible_moves = {}
         for move, delta in POSSIBLE_MOVES.items():
             new_x = self.my_head_x + delta[0]
             new_y = self.my_head_y + delta[1]
-            move_rank[move] = 0
-            print(f'possible move {move}=({new_x}, {new_y})')
             if self.is_out_of_bounds(new_x, new_y):
                 print(f'{move}=({new_x}, {new_y}) is out of bounds')
-                move_rank[move] += -100
                 continue
             if self.has_snake(new_x, new_y):
                 print(f'{move}=({new_x}, {new_y}) has snake')
-                move_rank[move] = -100
                 continue
-            if self.get_at(new_x, new_y) & FOOD:
-                print(f'{move}=({new_x}, {new_y}) has food')
-                if goal == GET_FOOD:
-                    move_rank[move] = 10
-                else:
-                    move_rank[move] = 0
-            if self.is_close_to_other_head(new_x, new_y):
+            possible_moves[move] = (new_x, new_y)
+        return possible_moves
+
+    def rank_moves(self, goal=STAY_ALIVE):
+        move_rank = {}
+        for move, (new_x, new_y) in self.get_possible_moves().items():
+            print(f'possible move {move}=({new_x}, {new_y})')
+            food_distance = self.get_at(new_x, new_y, board=self.food_board)
+            print(f'{move}=({new_x}, {new_y}) is {food_distance} away from food')
+            close_to_other_head = self.is_close_to_other_head(new_x, new_y)
+            if close_to_other_head:
                 print(f'{move}=({new_x}, {new_y}) is close to other head')
-                move_rank[move] -= 2
+            count_open_squares = self.count_open_squares(new_x, new_y)
+            print(
+                f'{move}=({new_x}, {new_y}) has {count_open_squares} open squares'
+            )
+            if goal==STARVING:
+                ranking_tuple = (food_distance, -count_open_squares, close_to_other_head)
+            else:
+                ranking_tuple = (-count_open_squares, food_distance, close_to_other_head)
+            move_rank[move] = ranking_tuple
         return move_rank
+
+    def count_open_squares(self, x, y):
+        return flood_count(self.board, x, y)
 
     def print_board(self):
         for i in range(len(self.board)):
@@ -136,13 +208,14 @@ class Game:
                     pos = '{:3d},'.format(self.board[i][j])
                 print(pos, end='')
             print()
-        
+
     def get_best_move(self):
         self.print_board()
         goal = STAY_ALIVE
-        if self.my_health < 50:
-            goal = GET_FOOD
+        if self.my_health < 20:
+            goal = STARVING
+        # lower is better
         moves = self.rank_moves(goal=goal)
-        best_moves = sorted(moves.items(), key=lambda m: -m[1])
+        best_moves = sorted(moves.items(), key=lambda m: m[1])
         print(best_moves)
         return best_moves[0][0]
