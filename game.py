9d389d564ca122ad684b582cd6cf020974f4fa65
Mohamed Heikal <mheikal@google.com>
2020-09-02 09:26:12 -0400

    MVP snake

--- game.py
+++ game.py
@@ -1,6 +1,79 @@
 #!/usr/bin/env python3
+
+DEFAULT_BOARD_SIZE = 11
+FOOD = 1 << 0
+SNAKE = 1 << 1
+IS_HEAD = 1 << 2
+MY_SNAKE = 1 << 3
+
+POSSIBLE_MOVES = {
+    'up': (0,1),
+    'down': (0,-1),
+    'left': (-1,0),
+    'right': (1,0),
+}
+
 class Game:
     def __init__(self, game_state):
-        pass
+        self.board_size = game_state['board']['height']
+        self.board = [[0]*self.board_size]*self.board_size
+        self._add_food(game_state['board']['food'])
+        self._add_snakes(game_state['board']['snakes'])
+        self._set_my_snake(game_state['board']['you'])
+
+    def _add_food(self, food_pos):
+        for pos in food_pos:
+            self.set_at(pos['x'], pos['y'], FOOD)
+    
+    def _add_snakes(self, snakes):
+        for snake in snakes:
+            for pos in snake['body']:
+                self.set_at(pos['x'], pos['y'], SNAKE)
+            head = snake['head']
+            self.set_at(head['x'], head['y'], HEAD, append=True)
+
+    def _set_my_snake(self, snake):
+        for pos in snake['body']:
+            self.set_at(pos['x'], pos['y'], MY_SNAKE, append=True)
+        self.my_health = snake['health']
+        self.my_head_x = snake['head']['x']
+        self.my_head_y = snake['head']['y']
+        self.my_snake = snake
+    
+    def to_board_idx(self, x, y):
+        board_y = self.board_size-y-1
+        board_x = x
+        return board_y, board_x
+    
+    def set_at(self, x, y, value, append=False):
+        i, j = self.to_board_idx(x, y)
+        if append:
+            self.board[i][j] |= value
+        else:
+            self.board[i][j] = value
+
+    def get_at(self, x, y):
+        i, j = self.to_board_idx(x, y)
+        return self.board[i][j]
+    
+    def get_safe_move(self):
+        for move, delta in POSSIBLE_MOVES.items():
+            new_x = self.head_x + delta[0]
+            new_y = self.head_y + delta[1]
+            if self.is_out_of_bounds(new_x, new_y):
+                continue
+            if self.is_empty(new_x, new_y):
+                return move
+    
+    def is_out_of_bounds(self, x, y):
+        if x < 0 or x > self.board_size:
+            return True
+        if y < 0 or y > self.board_size:
+            return True
+        return False
+    
+    def is_empty(self, x, y):
+        return self.get_at(x, y) == 0
+        
     def get_best_move(self):
-        return "right"
+        return self.get_safe_move()
--- server.py
+++ server.py
@@ -3,22 +3,13 @@ import random
 
 import cherrypy
 
-"""
-This is a simple Battlesnake server written in Python.
-For instructions see https://github.com/BattlesnakeOfficial/starter-snake-python/README.md
-"""
-
-
 class Battlesnake(object):
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def index(self):
-        # This function is called when you register your Battlesnake on play.battlesnake.com
-        # It controls your Battlesnake appearance and author permissions.
-        # TIP: If you open your Battlesnake URL in browser you should see this data
         return {
             "apiversion": "1",
-            "author": "",  # TODO: Your Battlesnake Username
+            "author": "mheikal",  # TODO: Your Battlesnake Username
             "color": "#888888",  # TODO: Personalize
             "head": "default",  # TODO: Personalize
             "tail": "default",  # TODO: Personalize
@@ -27,9 +18,6 @@ class Battlesnake(object):
     @cherrypy.expose
     @cherrypy.tools.json_in()
     def start(self):
-        # This function is called everytime your snake is entered into a game.
-        # cherrypy.request.json contains information about the game that's about to be played.
-        # TODO: Use this function to decide how your snake is going to look on the board.
         data = cherrypy.request.json
 
         print("START")
@@ -39,14 +27,8 @@ class Battlesnake(object):
     @cherrypy.tools.json_in()
     @cherrypy.tools.json_out()
     def move(self):
-        # This function is called on every turn of a game. It's how your snake decides where to move.
-        # Valid moves are "up", "down", "left", or "right".
-        # TODO: Use the information in cherrypy.request.json to decide your next move.
         data = cherrypy.request.json
 
-        # Choose a random direction to move in
-        possible_moves = ["up", "down", "left", "right"]
-        move = random.choice(possible_moves)
         move = Game(data).get_best_move()
 
         print(f"MOVE: {move}")
@@ -55,8 +37,6 @@ class Battlesnake(object):
     @cherrypy.expose
     @cherrypy.tools.json_in()
     def end(self):
-        # This function is called when a game your snake was in ends.
-        # It's purely for informational purposes, you don't have to make any decisions here.
         data = cherrypy.request.json
 
         print("END")
